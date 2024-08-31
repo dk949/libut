f9eb362cc776374efe49996f89d57af338805046
dk949 <56653556+dk949@users.noreply.github.com>
2024-08-31 03:23:12 +0100

    added documentation

--- include/resource/include/ut/resource/resource.hpp
+++ include/resource/include/ut/resource/resource.hpp
@@ -2,6 +2,67 @@
 #define UT_RESOURCE_HPP
 #include "optionalof.hpp"
 
+/* Generic resource holder with a destructor.
+ *
+ * Similar to std::uniqur_ptr, but:
+ *  1. Works with any kind of resource, not just pointers
+ *  2. Has better CTAD, including constructing a resource just from the constructor
+ *
+ * Usage:
+ *
+ * ```
+ *     value_t getValueResource();
+ *     void freeValueResource(value_t);
+ *
+ *     ptr_t getPtrResource();
+ *     void freePtrResource(ptr_t);
+ *     {
+ *         ut::Resource value1 {getValueResource(), freeValueResource};
+ *         ut::Resource value2 {freeValueResource};
+ *         // `freeValueResource` will be called at the end of the scope for
+ *         // value1 but not value2, because value2 has not been initialised.
+ *     }
+ *
+ *     {
+ *         ut::Resource value1 {getValueResource(), freeValueResource};
+ *         ut::Resource value2 {freeValueResource};
+ *
+ *         value1.release(); // `freeValueResource` called here
+ *
+ *         value2.acquire(getValueResource());
+ *
+ *         // `freeValueResource` called for old resource here
+ *         // and will be called again at the end of the scope for
+ *         // the resource aquired here.
+ *         value2.acquire(getValueResource());
+ *
+ *     }
+ *
+ *     {
+ *         ut::Resource value {getValueResource(), freeValueResource};
+ *         ut::Resource pointer {getPtrResource(), freePtrResource};
+ *
+ *         // `free*` functions will not be called
+ *         // optional may be empty and pointer may be null!
+ *         std::optional<value_t> value.takeOwnership();
+ *         ptr_t pointer.takeOwnership();
+ *     }
+ *
+ *     {
+ *         ut::Resource value {getValueResource(), freeValueResource};
+ *         ut::Resource pointer {getPtrResource(), freePtrResource};
+ *
+ *         value_t &v = value.get(); // UB if value is empty
+ *         ptr_t p = ptr.get();      // pointer may be null
+ *         auto x = value->x;        // UB if value is empty
+ *         auto x = ptr->x;          // UB if value is empty
+ *
+ *         // Check if value is empty with `Resource::hasValue()`
+ *     }
+ *
+ * ```
+ */
+
 namespace ut {
 
 template<typename T, typename D>
@@ -41,11 +102,23 @@ public:
         return isNull<T>();
     }
 
+    /**
+     * Acquire  the resource.
+     *
+     * If resource was previously not empty, release it first
+     */
     void acquire(T &&data) noexcept(noexcept(release()) && std::is_nothrow_move_assignable_v<T>) {
         release();
         m_data = std::move(data);
     }
 
+    /**
+     * Release the resource
+     *
+     * No-op  if resource was empty.
+     *
+     * NOTE: this is called automatically by the destructor
+     */
     void release() noexcept(noexcept(m_destructor(getNonNullValue<T>(m_data)))
                             && std::is_nothrow_assignable_v<T, decltype(nullValueOf<T>())>) {
         if (!isNull<T>(m_data)) {
@@ -54,7 +127,14 @@ public:
         }
     }
 
-    [[nodiscard]]
+    /**
+     * Take ownership of the resource.
+     *
+     * The resource will *NOT* be automatically released by the destructor.
+     *
+     * Resource becomes empty.
+     */
+    [[nodiscard("If return value is discarded, resource may be leaked")]]
     OptionalOf<T> takeOwnership() noexcept(std::is_nothrow_move_assignable_v<T>  //
                                            && std::is_nothrow_assignable_v<T, decltype(nullValueOf<T>())>) {
         auto tmp = std::move(m_data);
@@ -62,21 +142,41 @@ public:
         return tmp;
     }
 
+    /**
+     * Get a non-owning reference to the resource
+     *
+     * *UB if called on empty resource*, use `hasValue` to check first.
+     */
     [[nodiscard]]
     T &get() noexcept {
         return getNonNullValue<T>(m_data);
     }
 
+    /**
+     * Get a non-owning reference to the resource
+     *
+     * *UB if called on empty resource*, use `hasValue` to check first.
+     */
     [[nodiscard]]
     T const &get() const noexcept {
         return getNonNullValue<T>(m_data);
     }
 
+    /**
+     * Call `operator->` on the underlying value/pointer
+     *
+     * *UB if called on empty resource*, use `hasValue` to check first.
+     */
     [[nodiscard]]
     PointerOf<T> operator->() noexcept {
         return operatorArrow<T>(m_data);
     }
 
+    /**
+     * Call `operator->` on the underlying value/pointer
+     *
+     * *UB if called on empty resource*, use `hasValue` to check first.
+     */
     [[nodiscard]]
     std::conditional_t<needs_internal_mutability, PointerOf<T>, ConstPointerOf<T>> operator->() const noexcept {
         return operatorArrow<T>(m_data);
