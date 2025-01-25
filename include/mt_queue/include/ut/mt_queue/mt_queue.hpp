6c9c2ebc340b105fb69078386632e2478a18bee5
dk949 <56653556+dk949@users.noreply.github.com>
2025-01-25 21:38:38 +0000

    multi threaded queue

---
+++ include/mt_queue/include/ut/mt_queue/mt_queue.hpp
@@ -0,0 +1,247 @@
+#ifndef UT_MT_QUEUE_HPP
+#define UT_MT_QUEUE_HPP
+
+#include <condition_variable>
+#include <deque>
+#include <mutex>
+#include <optional>
+#include <queue>
+
+namespace ut {
+template<typename T>
+struct is_queue_like : std::false_type { };
+
+template<typename T, typename Container>
+struct is_queue_like<std::queue<T, Container>> : std::true_type { };
+
+template<typename T, typename Container, typename Compare>
+struct is_queue_like<std::priority_queue<T, Container, Compare>> : std::true_type { };
+
+template<typename T, typename Allocator>
+struct is_queue_like<std::deque<T, Allocator>> : std::true_type { };
+
+template<typename T>
+inline constexpr bool is_queue_like_v = is_queue_like<T>::value;
+
+template<typename T>
+concept QueueLike = is_queue_like_v<T>;
+
+/**
+ * A Multi-threading safe queue.
+ *
+ * All public member functions are safe to call from multiple different threads.
+ *
+ * *Not* lock free and generally *not* suitable for high performance applications.
+ *
+ * All inputs and outputs are by value, to simplify synchronisation.
+ *
+ * By default uses `std::queue` as the underlying container.
+ * Can also use `std::priority_queue` or `std::deque`.
+ *
+ * NOTE: If the queue is a priority_queue, it cannot be used with move-only objects.
+ *
+ *       Move-only objects also disable all front* methods, as they require copying the element.
+ *
+ *       std::shared_ptr can be used to avoid this issue, but it is not recommended. (also note that the
+ *       default comparitor for a shared_ptr in a priority_queue is not very useful)
+ */
+
+template<typename T, QueueLike Q = std::queue<T>>
+class MtQueue {
+public:
+    /**
+     * Push an item to the back of the queue
+     */
+    void push(T t) {
+        {
+            std::lock_guard<std::mutex> g {m_mu};
+            pushImpl(std::move(t));
+        }
+        m_cv.notify_one();
+    }
+
+    /**
+     * Removes an item from the front of the queue and returns it.
+     *
+     * If the queue is empty, blocks until it is not empty.
+     *
+     * Moves the item if this is supported (i.e. not when using a `priority_queue`)
+     */
+    T pop() {
+        std::unique_lock<std::mutex> g {m_mu};
+        m_cv.wait(g, [this] { return !m_q.empty(); });
+        return popImpl();
+    }
+
+    /**
+     * Just like `pop`, but if the queue is empty, returns `std::nullopt`
+     */
+    std::optional<T> tryPop() {
+        std::lock_guard<std::mutex> g {m_mu};
+        if (m_q.empty()) return std::nullopt;
+        return popImpl();
+    }
+
+    /**
+     * Like `tryPop`, but waits for `duration` before returning `std::nullopt`
+     */
+    template<class Rep, class Period>
+    std::optional<T> tryPopFor(std::chrono::duration<Rep, Period> const &duration) {
+        std::unique_lock<std::mutex> g {m_mu};
+        if (m_cv.wait_for(g, duration, [this]() { return !m_q.empty(); })) return popImpl();
+        return std::nullopt;
+    }
+
+    /**
+     * Take a copy of the front of the queue.
+     *
+     * Blocks until queue is not empty
+     *
+     * Does not remove item from the queue.
+     */
+    [[nodiscard]]
+    T front() {
+        std::unique_lock<std::mutex> g {m_mu};
+        m_cv.wait(g, [this] { return !m_q.empty(); });
+        return frontImpl();
+    }
+
+    /**
+     * Take a copy of the front of the queue.
+     *
+     * Blocks until queue is not empty
+     *
+     * Does not remove item from the queue
+     */
+    [[nodiscard]]
+    T front() const {
+        std::unique_lock<std::mutex> g {m_mu};
+        m_cv.wait(g, [this] { return !m_q.empty(); });
+        return frontImpl();
+    }
+
+    /**
+     * Like `front`, but if the queue is empty, returns `std::nullopt`
+     */
+    [[nodiscard]]
+    std::optional<T> tryFront() {
+        std::lock_guard<std::mutex> g {m_mu};
+        if (m_q.empty()) return std::nullopt;
+        return frontImpl();
+    }
+
+    /**
+     * Like `front`, but if the queue is empty, returns `std::nullopt`
+     */
+    [[nodiscard]]
+    std::optional<T> tryFront() const {
+        std::lock_guard<std::mutex> g {m_mu};
+        if (m_q.empty()) return std::nullopt;
+        return frontImpl();
+    }
+
+    /**
+     * Like `tryFront`, but waits for `duration` before returning `std::nullopt`
+     */
+    template<class Rep, class Period>
+    [[nodiscard]]
+    std::optional<T> tryFrontFor(std::chrono::duration<Rep, Period> dur) {
+        std::unique_lock<std::mutex> g {m_mu};
+        if (m_cv.wait_for(g, dur, [this]() { return !m_q.empty(); })) return frontImpl();
+        return std::nullopt;
+    }
+
+    /**
+     * Like `tryFront`, but waits for `duration` before returning `std::nullopt`
+     */
+    template<class Rep, class Period>
+    [[nodiscard]]
+    std::optional<T> tryFrontFor(std::chrono::duration<Rep, Period> dur) const {
+        std::unique_lock<std::mutex> g {m_mu};
+        if (m_cv.wait_for(g, dur, [this]() { return !m_q.empty(); })) return frontImpl();
+        return std::nullopt;
+    }
+
+    /**
+     * Is the queue empty.
+     */
+    [[nodiscard]]
+    bool empty() const {
+        std::lock_guard<std::mutex> g {m_mu};
+        return m_q.empty();
+    }
+private:
+    mutable std::mutex m_mu;
+    mutable std::condition_variable m_cv;
+    Q m_q;
+    static constexpr bool has_top = requires {
+        { m_q.top() } -> std::same_as<T const &>;
+    };
+    static constexpr bool has_front = requires {
+        { m_q.front() } -> std::same_as<T const &>;
+    }
+    || requires {
+        { m_q.front() } -> std::same_as<T &>;
+    };
+    static constexpr bool has_push = requires(T t) {
+        m_q.push(t);
+    };
+    static constexpr bool has_push_back = requires(T t) {
+        m_q.push_back(t);
+    };
+    static constexpr bool has_pop = requires {
+        m_q.pop();
+    };
+    static constexpr bool has_pop_front = requires {
+        m_q.pop_front();
+    };
+
+    static_assert(has_top != has_front, "Queue has to have either top or front, but not both");
+    static_assert(has_push != has_push_back, "Queue has to have either push or push_back, but not both");
+    static_assert(has_pop != has_pop_front, "Queue has to have either pop or pop_front, but not both");
+
+    [[nodiscard]]
+    decltype(auto) frontImpl() requires(has_top) {
+        return m_q.top();
+    }
+
+    [[nodiscard]]
+    decltype(auto) frontImpl() const requires(has_top) {
+        return m_q.top();
+    }
+
+    [[nodiscard]]
+    decltype(auto) frontImpl() requires(has_front) {
+        return m_q.front();
+    }
+
+    [[nodiscard]]
+    decltype(auto) frontImpl() const requires(has_front) {
+        return m_q.front();
+    }
+
+    [[nodiscard]]
+    T popImpl() requires(has_pop) {
+        auto t = std::move(frontImpl());
+        m_q.pop();
+        return t;
+    }
+
+    [[nodiscard]]
+    T popImpl() requires(has_pop_front) {
+        auto t = std::move(frontImpl());
+        m_q.pop_front();
+        return t;
+    }
+
+    void pushImpl(T &&t) requires(has_push) {
+        m_q.push(std::move(t));
+    }
+
+    void pushImpl(T &&t) requires(has_push_back) {
+        m_q.push_back(std::move(t));
+    }
+};
+}  // namespace ut
+
+#endif  // UT_MT_QUEUE_HPP
---
+++ tests/mt_queue.cpp
@@ -0,0 +1,219 @@
+//  NOLINTBEGIN(readability-magic-numbers,google-build-using-namespace)
+#include <ut/mt_queue/mt_queue.hpp>
+
+#include <barrier>
+#include <chrono>
+#include <numeric>
+#include <thread>
+namespace chr = std::chrono;
+using namespace std::chrono_literals;
+
+#include <catch.hpp>
+using namespace ut;
+
+TEMPLATE_TEST_CASE("Single threaded", "[mt_queue]", std::queue<int>, std::deque<int>, std::priority_queue<int>) {
+    MtQueue<int, TestType> q;
+    SECTION("push, front, pop, empty") {
+        // NOTE: pushing in order that will be preserved by the priority queueu
+        q.push(30);
+        q.push(20);
+        q.push(10);
+
+        REQUIRE(!q.empty());
+        REQUIRE(q.front() == 30);
+        REQUIRE(q.pop() == 30);
+        REQUIRE(q.front() == 20);
+        REQUIRE(q.pop() == 20);
+        REQUIRE(q.front() == 10);
+        REQUIRE(q.pop() == 10);
+        REQUIRE(q.empty());
+    }
+
+
+    SECTION("tryPop") {
+        REQUIRE(!q.tryPop().has_value());
+        q.push(7);
+        auto p = q.tryPop();
+        REQUIRE(p.has_value());
+        REQUIRE(*p == 7);
+    }
+
+    SECTION("tryFront") {
+        REQUIRE(!q.tryFront().has_value());
+        q.push(7);
+        auto p = q.tryFront();
+        REQUIRE(p.has_value());
+        REQUIRE(*p == 7);
+    }
+
+    SECTION("tryPopFor") {
+        auto now = chr::high_resolution_clock::now();
+        REQUIRE(!q.tryPopFor(50ms).has_value());
+        REQUIRE(chr::high_resolution_clock::now() - now >= 50ms);
+        q.push(7);
+        now = chr::high_resolution_clock::now();
+        auto p = q.tryPopFor(50ms);
+        REQUIRE(chr::high_resolution_clock::now() - now < 50ms);
+        REQUIRE(p.has_value());
+        REQUIRE(*p == 7);
+    }
+
+    SECTION("tryFrontFor") {
+        auto now = chr::high_resolution_clock::now();
+        REQUIRE(!q.tryFrontFor(50ms).has_value());
+        REQUIRE(chr::high_resolution_clock::now() - now >= 50ms);
+        q.push(7);
+        now = chr::high_resolution_clock::now();
+        auto p = q.tryFrontFor(50ms);
+        REQUIRE(chr::high_resolution_clock::now() - now < 50ms);
+        REQUIRE(p.has_value());
+        REQUIRE(*p == 7);
+    }
+}
+
+TEMPLATE_TEST_CASE("Multi-threaded", "[mt_queue]", std::queue<int>, std::deque<int>, std::priority_queue<int>) {
+    MtQueue<int, TestType> q;
+
+    SECTION("pop, front") {
+        std::barrier b {2};
+        auto producer = std::jthread([&q, &b]() {
+            REQUIRE(q.empty());
+            b.arrive_and_wait();
+            std::this_thread::sleep_for(30ms);
+            q.push(30);
+            std::this_thread::sleep_for(30ms);
+            q.push(20);
+        });
+        auto consumer = std::jthread([&q, &b]() {
+            b.arrive_and_wait();
+            auto now = chr::high_resolution_clock::now();
+            REQUIRE(q.front() == 30);
+            REQUIRE(!q.empty());
+            REQUIRE(std::chrono::high_resolution_clock::now() - now >= 30ms);
+            REQUIRE(q.pop() == 30);
+            REQUIRE(q.pop() == 20);
+            REQUIRE(std::chrono::high_resolution_clock::now() - now >= 60ms);
+        });
+    }
+    SECTION("tryPop") {
+        auto producer = std::jthread([&q]() {
+            std::this_thread::sleep_for(30ms);
+            q.push(30);
+        });
+        auto consumer = std::jthread([&q]() {
+            REQUIRE(!q.tryFront().has_value());
+            REQUIRE(!q.tryPop().has_value());
+            std::this_thread::sleep_for(40ms);
+            auto f = q.tryFront();
+            auto p = q.tryPop();
+            REQUIRE(f.has_value());
+            REQUIRE(*f == 30);
+            REQUIRE(p.has_value());
+            REQUIRE(*p == 30);
+        });
+    }
+}
+
+TEMPLATE_TEST_CASE("2 threaded stress test", "[mt_queue]", std::queue<int>, std::deque<int>, std::priority_queue<int>) {
+    MtQueue<int, TestType> q;
+
+    auto producer = std::jthread([&q]() {
+        for (int i = 10000; i > 0; --i) {
+            q.push(i);
+        }
+    });
+    auto consumer = std::jthread([&q]() {
+        for (int i = 10000; i > 0; --i) {
+            REQUIRE(q.front() == i);
+            REQUIRE(q.pop() == i);
+        }
+    });
+}
+
+TEMPLATE_TEST_CASE("multi producer single consumer stress test", "[mt_queue]", std::queue<int>, std::deque<int>
+    // , std::priority_queue<int>
+) {
+    MtQueue<int, TestType> q;
+
+
+    std::stop_source stop;
+
+    auto producer = std::jthread([&q, &stop]() {
+        for (int i = 10000; i > 0; --i) {
+            q.push(i);
+        }
+        stop.request_stop();
+    });
+    std::vector<std::jthread> consumers;
+    std::vector<std::vector<int>> consumer_fronts {10};
+    std::vector<std::vector<int>> consumer_pops {10};
+    for (size_t i = 0; i < 10; ++i) {
+        consumers.emplace_back([&q, &pops = consumer_pops[i], &fronts = consumer_fronts[i], token = stop.get_token()]() {
+            while (!(token.stop_requested() && q.empty())) {
+                auto const front = q.tryFront();
+                if (!front.has_value()) continue;
+                fronts.push_back(*front);
+                auto const pop = q.tryPop();
+                if (!pop.has_value()) continue;
+                pops.push_back(*pop);
+            }
+        });
+    }
+
+    producer.join();
+    for (auto &c : consumers) {
+        c.join();
+    }
+
+    REQUIRE(q.empty());
+    REQUIRE(consumer_fronts.size() == consumer_pops.size());
+    for (size_t i = 0; i < consumer_fronts.size(); ++i) {
+        auto const &fronts = consumer_fronts[i];
+        auto const &pops = consumer_pops[i];
+        for (size_t j = 0; j < std::min(fronts.size(), pops.size()); ++j) {
+            REQUIRE(fronts[j] >= pops[j]);
+        }
+    }
+
+
+    auto const sum_pops =
+        std::accumulate(consumer_pops.begin(), consumer_pops.end(), std::size_t {}, [](size_t sum, auto const &v) {
+        return sum + v.size();
+    });
+
+    // Not checking this for fronts. sum of all fronts sizes can be greater than 10000 if a successful tryFront is
+    // followed by an unsuccessful tryPop and then another successful tryFront.
+    REQUIRE(sum_pops == 10000);
+}
+
+TEMPLATE_TEST_CASE("Single trheaded using unique_ptr",
+    "[mt_queue]",
+    std::queue<std::unique_ptr<int>>,
+    std::deque<std::unique_ptr<int>>) {
+    MtQueue<std::unique_ptr<int>, TestType> q;
+
+    q.push(std::make_unique<int>(30));
+    q.push(std::make_unique<int>(20));
+    q.push(std::make_unique<int>(10));
+    REQUIRE(*q.pop() == 30);
+    REQUIRE(*q.pop() == 20);
+    REQUIRE(*q.pop() == 10);
+}
+
+TEMPLATE_TEST_CASE("Single trheaded using shared_ptr",
+    "[mt_queue]",
+    std::queue<std::shared_ptr<int>>,
+    std::deque<std::shared_ptr<int>>,
+    std::priority_queue<std::shared_ptr<int>>) {
+
+    MtQueue<std::shared_ptr<int>, TestType> q;
+
+    q.push(std::make_shared<int>(30));
+    auto front = q.front();
+    REQUIRE(*q.pop() == 30);
+    REQUIRE(q.empty());
+    // pointer still valid after queue is empty
+    REQUIRE(*front == 30);
+}
+
+//  NOLINTEND(readability-magic-numbers,google-build-using-namespace)
