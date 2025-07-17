4ad53da607c601a5bf4f26f06294080b8ea56e2e
dk949 <56653556+dk949@users.noreply.github.com>
2025-07-17 18:48:48 +0100

    WIP: more work on switchboard

--- include/switchboard/include/ut/switchboard/switchboard.hpp
+++ include/switchboard/include/ut/switchboard/switchboard.hpp
@@ -18,14 +18,28 @@ struct Name {
 };
 
 template<StaticString str>
+struct Desc {
+    static constexpr auto desc = str;
+};
+
+template<>
+struct Desc<""> {
+    static constexpr auto desc = "";
+};
+
+template<StaticString str>
 constexpr auto name = Name<str> {};
+
+template<StaticString str>
+constexpr auto desc = Desc<str> {};
 
 namespace detail {
 
-    template<std::size_t str_size, std::size_t name_size>
+    template<std::size_t str_size, std::size_t name_size, std::size_t desc_size>
     struct Conf {
         StaticString<str_size> str = "";
         StaticString<name_size> name = "";
+        StaticString<desc_size> desc = "";
 
         template<std::size_t S>
         constexpr Conf(char const (&s)[S])
@@ -34,13 +48,20 @@ namespace detail {
         template<StaticString s>
         constexpr Conf(Name<s> n)
                 : name(n.name) { }
+
+        template<StaticString s>
+        constexpr Conf(Desc<s> d)
+                : desc(d.desc) { }
     };
 
     template<std::size_t S>
-    Conf(char const (&)[S]) -> Conf<S, 1>;
+    Conf(char const (&)[S]) -> Conf<S, 1, 1>;
 
     template<StaticString s>
-    Conf(Name<s> n) -> Conf<1, s.size()>;
+    Conf(Name<s> n) -> Conf<1, s.size(), 1>;
+
+    template<StaticString s>
+    Conf(Desc<s> n) -> Conf<1, 1, s.size()>;
 
     template<Conf conf>
     struct NthConfBase {
@@ -73,19 +94,21 @@ struct Arg {
     using type = T;
     std::optional<std::string_view> short_flag;
     std::optional<std::string_view> long_flag;
-    std::string_view name;
+    std::optional<std::string_view> name;
+    std::optional<std::string_view> description;
 
     consteval Arg() {
         constexpr auto maybe_short = detail::nthConf<0, conf...>.str;
         UT_SW_DETAIL_ASSERT(!maybe_short.empty(), "First argument must be a non-empty string", "defining an `Arg`");
-        constexpr auto short_res = getShort();
+        constexpr auto short_res = getShort<maybe_short>();
         short_flag = short_res.first;
-        // constexpr std::string_view maybe_long = detail::nthConf<0, conf...>.str.view();
-        // UT_SW_DETAIL_ASSERT(!maybe_short.empty(), "First argument must be a non-empty string", "defining an `Arg`");
+        constexpr auto maybe_long = detail::nthConf<short_res.second, conf...>.str;
+        if constexpr (maybe_long.empty()) { }
     };
 private:
-    constexpr std::pair<std::optional<std::string_view>, std::size_t> getShort() {
-        return std::pair {std::nullopt, 0u};
+    template<StaticString maybe_short>
+    static constexpr std::pair<std::optional<std::string_view>, std::size_t> getShort() {
+        return std::pair {std::nullopt, std::size_t(0)};
     }
 };
 
--- tests/switchboard.cpp
+++ tests/switchboard.cpp
@@ -5,8 +5,8 @@
 TEST_CASE("Switch board", "[switchboard]") {
 
     using namespace ut::sw;
-    using force = Arg<bool, "-f", "--force">;
-    using output = Arg<std::string, "-o", name<"output">>;
-    using input = Arg<std::optional<std::string>, "-i", name<"input">>;
+    using force = Arg<bool, "-f", "--force", desc<"kjhk">>;
+    using output = Arg<std::string, "-o", name<"output">, desc<"desc">>;
+    using input = Arg<std::optional<std::string>, "-i", name<"input">, desc<"aaaaaa">>;
     ut::sw::ArgParser<force, output, input, Arg<bool, "-v", "--verbose">> parser;
 }
