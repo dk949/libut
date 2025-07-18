501449ab8c843f7c3c11423c68c98f03d5bcd772
dk949 <56653556+dk949@users.noreply.github.com>
2025-07-18 04:01:38 +0100

    got basic configuration down for switchboard

--- include/switchboard/include/ut/switchboard/switchboard.hpp
+++ include/switchboard/include/ut/switchboard/switchboard.hpp
@@ -5,8 +5,8 @@
 #include <cstdlib>
 #include <optional>
 #include <type_traits>
-#if __cplusplus < 202'002L
-#    error this file has to be compiled with at least C++20
+#if __cplusplus < 202'302L
+#    error this file has to be compiled with at least C++23
 #endif
 #include <ut/static_string/static_string.hpp>
 
@@ -20,11 +20,6 @@ struct Name {
 template<StaticString str>
 struct Desc {
     static constexpr auto desc = str;
-};
-
-template<>
-struct Desc<""> {
-    static constexpr auto desc = "";
 };
 
 template<StaticString str>
@@ -52,6 +47,10 @@ namespace detail {
         template<StaticString s>
         constexpr Conf(Desc<s> d)
                 : desc(d.desc) { }
+
+        constexpr bool empty() const {
+            return str.empty() && name.empty() && desc.empty();
+        }
     };
 
     template<std::size_t S>
@@ -79,55 +78,155 @@ namespace detail {
     template<std::size_t n, Conf... rest>
     static constexpr auto nthConf = NthConf<n, rest...>::value;
 
-#define UT_SW_DETAIL_ASSERT(b, msg, when) \
-    static_assert((b),                    \
-        "\n\n"                            \
-        "SwitchBoard error!\n"            \
-        "\n" msg "\n"                     \
-        "This error occurred when " when "\n\n")
-
+#define UT_SW_DETAIL_MAKE_MESSAGE(msg, when) \
+    "\n\n"                                   \
+    "SwitchBoard error!\n"                   \
+    "\n" msg "\n"                            \
+    "This error occurred when " when "\n\n"
+#define UT_SW_DETAIL_STATIC_ASSERT(b, msg, when) static_assert((b), UT_SW_DETAIL_MAKE_MESSAGE(msg, when))
+#define UT_SW_DETAIL_ARG_ASSERT(b, msg, pos)                                               \
+    do {                                                                                   \
+        if constexpr (pos == 0) {                                                          \
+            UT_SW_DETAIL_STATIC_ASSERT(b, msg, "defining an `Arg` (1st config option).");  \
+        } else if constexpr (pos == 1) {                                                   \
+            UT_SW_DETAIL_STATIC_ASSERT(b, msg, "defining an `Arg` (2nd config option).");  \
+        } else if constexpr (pos == 2) {                                                   \
+            UT_SW_DETAIL_STATIC_ASSERT(b, msg, "defining an `Arg` (3rd config option).");  \
+        } else if constexpr (pos == 3) {                                                   \
+            UT_SW_DETAIL_STATIC_ASSERT(b, msg, "defining an `Arg` (4th config option).");  \
+        } else if constexpr (pos == 4) {                                                   \
+            UT_SW_DETAIL_STATIC_ASSERT(b, msg, "defining an `Arg` (5th config option).");  \
+        } else if constexpr (pos == 5) {                                                   \
+            UT_SW_DETAIL_STATIC_ASSERT(b, msg, "defining an `Arg` (6th config option).");  \
+        } else if constexpr (pos == 6) {                                                   \
+            UT_SW_DETAIL_STATIC_ASSERT(b, msg, "defining an `Arg` (7th config option).");  \
+        } else if constexpr (pos == 7) {                                                   \
+            UT_SW_DETAIL_STATIC_ASSERT(b, msg, "defining an `Arg` (8th config option).");  \
+        } else if constexpr (pos == 8) {                                                   \
+            UT_SW_DETAIL_STATIC_ASSERT(b, msg, "defining an `Arg` (9th config option).");  \
+        } else if constexpr (pos == 9) {                                                   \
+            UT_SW_DETAIL_STATIC_ASSERT(b, msg, "defining an `Arg` (10th config option)."); \
+        } else {                                                                           \
+            UT_SW_DETAIL_STATIC_ASSERT(false,                                              \
+                "(internal) Too many config options for `Arg`",                            \
+                "handling Conf position printing in static_assert.");                      \
+        }                                                                                  \
+    } while (0)
 
 }  // namespace detail
 
 template<typename T, detail::Conf... conf>
 struct Arg {
     using type = T;
-    std::optional<std::string_view> short_flag;
-    std::optional<std::string_view> long_flag;
-    std::optional<std::string_view> name;
-    std::optional<std::string_view> description;
+    static constexpr auto out_of_args = ~std::size_t(0);
+    std::optional<std::string_view> short_flag = std::nullopt;
+    std::optional<std::string_view> long_flag = std::nullopt;
+    std::optional<std::string_view> name = std::nullopt;
+    std::optional<std::string_view> description = std::nullopt;
 
     consteval Arg() {
-        constexpr auto maybe_short = detail::nthConf<0, conf...>.str;
-        UT_SW_DETAIL_ASSERT(!maybe_short.empty(), "First argument must be a non-empty string", "defining an `Arg`");
-        constexpr auto short_res = getShort<maybe_short>();
+        constexpr auto short_res = getShort<0>();
         short_flag = short_res.first;
-        constexpr auto maybe_long = detail::nthConf<short_res.second, conf...>.str;
-        if constexpr (maybe_long.empty()) { }
+        constexpr auto long_res = getLong<short_res.second>();
+        long_flag = long_res.first;
+        parseRest<long_res.second, long_res.first.has_value()>();
+
+        UT_SW_DETAIL_STATIC_ASSERT(long_res.first || short_res.first,
+            "Either a long (e.g. --flag) or short (e.g. -f) flag must be provided.",
+            "defining an `Arg`");
+        if (!name) name = long_flag.value().substr(2);
     };
 private:
-    template<StaticString maybe_short>
+    template<std::size_t pos>
     static constexpr std::pair<std::optional<std::string_view>, std::size_t> getShort() {
-        return std::pair {std::nullopt, std::size_t(0)};
+        if constexpr (pos >= sizeof...(conf))
+            return std::pair {std::nullopt, out_of_args};
+        else {
+            static constexpr auto maybe_short = detail::nthConf<pos, conf...>;
+            UT_SW_DETAIL_ARG_ASSERT(!maybe_short.empty(), "Expected all strings to be non-empty", pos);
+            if constexpr (maybe_short.str.size() == 2 && maybe_short.str.starts_with('-'))
+                return std::pair {maybe_short.str.view(), std::size_t(pos + 1)};
+            return std::pair {std::nullopt, std::size_t(pos)};
+        }
+    }
+
+    template<std::size_t pos>
+    static constexpr std::pair<std::optional<std::string_view>, std::size_t> getLong() {
+        if constexpr (pos >= sizeof...(conf))
+            return std::pair {std::nullopt, out_of_args};
+        else {
+            static constexpr auto maybe_long = detail::nthConf<pos, conf...>;
+            UT_SW_DETAIL_ARG_ASSERT(!maybe_long.empty(), "Expected all strings to be non-empty", pos);
+            if constexpr (maybe_long.str.size() > 2 && maybe_long.str.starts_with("--"))
+                return std::pair {maybe_long.str.view(), std::size_t(pos + 1)};
+
+            return std::pair {std::nullopt, std::size_t(pos)};
+        }
+    }
+
+    template<std::size_t pos>
+    static constexpr std::pair<std::optional<std::string_view>, std::size_t> getName() {
+        if constexpr (pos >= sizeof...(conf))
+            return std::pair {std::nullopt, out_of_args};
+        else {
+            static constexpr auto maybe_name = detail::nthConf<pos, conf...>;
+            UT_SW_DETAIL_ARG_ASSERT(!maybe_name.empty(), "Expected all strings to be non-empty", pos);
+            if constexpr (!maybe_name.name.empty()) return std::pair {maybe_name.name.view(), pos + 1};
+
+            return std::pair {std::nullopt, pos};
+        }
+    }
+
+    template<std::size_t pos>
+    static constexpr std::pair<std::optional<std::string_view>, std::size_t> getDesc() {
+        if constexpr (pos >= sizeof...(conf))
+            return std::pair {std::nullopt, out_of_args};
+        else {
+            static constexpr auto maybe_desc = detail::nthConf<pos, conf...>;
+            UT_SW_DETAIL_ARG_ASSERT(!maybe_desc.empty(), "Expected all strings to be non-empty", pos);
+            if constexpr (!maybe_desc.desc.empty()) return std::pair {maybe_desc.desc.view(), pos + 1};
+
+            return std::pair {std::nullopt, pos};
+        }
+    }
+
+    template<std::size_t pos, bool has_long, bool has_name = false>
+    constexpr void parseRest() {
+        if constexpr (pos >= sizeof...(conf)) {
+            UT_SW_DETAIL_STATIC_ASSERT(has_long || has_name,
+                "Either a long flag (e.g. --flag) or a `name<...>` must be provided.",
+                "defining an `Arg`");
+            // if(!description) throw 1;
+            return;
+        } else {
+            constexpr auto name_res = getName<pos>();
+            if constexpr (name_res.first) name = name_res.first;
+            constexpr auto desc_res = getDesc<name_res.second>();
+            if constexpr (desc_res.first) description = desc_res.first;
+            UT_SW_DETAIL_ARG_ASSERT(name_res.first || desc_res.first, "Could not parse argument", pos);
+            return parseRest<desc_res.second, has_long, has_name || name_res.first.has_value()>();
+        }
     }
 };
 
-template<typename A>
-struct is_arg : std::false_type { };
+namespace detail {
+    template<typename A>
+    struct is_arg : std::false_type { };
 
-template<typename T, detail::Conf... def>
-struct is_arg<Arg<T, def...>> : std::true_type { };
+    template<typename T, detail::Conf... def>
+    struct is_arg<Arg<T, def...>> : std::true_type { };
+}
 
 template<typename T, detail::Conf... defs>
 static constexpr auto arg = Arg<T, defs...> {};
 
 template<typename... T>
 struct ArgParser {
-    static_assert((is_arg<T>::value && ...));
+    static_assert((detail::is_arg<T>::value && ...));
+    std::tuple<T...> args;
 
-    consteval ArgParser() {
-        (T {}, ...);
-    }
+    consteval ArgParser()
+            : args(T {}...) { }
 };
 
 }  // namespace ut::sw
--- tests/switchboard.cpp
+++ tests/switchboard.cpp
@@ -2,11 +2,29 @@
 #include <catch2/catch_test_macros.hpp>
 #include <ut/switchboard/switchboard.hpp>
 
-TEST_CASE("Switch board", "[switchboard]") {
+TEST_CASE("Switch board smoke test", "[switchboard]") {
 
     using namespace ut::sw;
-    using force = Arg<bool, "-f", "--force", desc<"kjhk">>;
-    using output = Arg<std::string, "-o", name<"output">, desc<"desc">>;
-    using input = Arg<std::optional<std::string>, "-i", name<"input">, desc<"aaaaaa">>;
-    ut::sw::ArgParser<force, output, input, Arg<bool, "-v", "--verbose">> parser;
+    using force = Arg<bool, "-f", "--force", desc<"force action">>;
+    using output = Arg<std::string, "-o", name<"output">>;
+    constexpr ArgParser<force, output, Arg<bool, "-v", "--verbose">> parser;
+
+    STATIC_REQUIRE(std::get<0>(parser.args).short_flag == "-f");
+    STATIC_REQUIRE(std::get<0>(parser.args).long_flag == "--force");
+    STATIC_REQUIRE(std::get<0>(parser.args).name == "force");
+    STATIC_REQUIRE(std::get<0>(parser.args).description == "force action");
+
+    STATIC_REQUIRE(std::get<1>(parser.args).short_flag == "-o");
+    STATIC_REQUIRE(!std::get<1>(parser.args).long_flag);
+    STATIC_REQUIRE(std::get<1>(parser.args).name == "output");
+    STATIC_REQUIRE(!std::get<1>(parser.args).description);
+}
+
+TEST_CASE("Switch board out of order", "[switchboard]") {
+    using namespace ut::sw;
+    constexpr ut::sw::ArgParser<Arg<std::optional<std::string>, "-i", "--in", desc<"input file">, name<"input">>> parser;
+    STATIC_REQUIRE(std::get<0>(parser.args).short_flag == "-i");
+    STATIC_REQUIRE(std::get<0>(parser.args).long_flag == "--in");
+    STATIC_REQUIRE(std::get<0>(parser.args).name == "input");
+    STATIC_REQUIRE(std::get<0>(parser.args).description == "input file");
 }
