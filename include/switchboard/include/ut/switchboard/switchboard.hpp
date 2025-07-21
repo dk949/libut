96d0193272cf33c1f70d04229da7ce83be56156f
dk949 <56653556+dk949@users.noreply.github.com>
2025-07-21 02:59:20 +0100

    WIP: switchboard segfaults

--- include/switchboard/include/ut/switchboard/switchboard.hpp
+++ include/switchboard/include/ut/switchboard/switchboard.hpp
@@ -1,17 +1,97 @@
 #ifndef UT_SWITCHBOARD_HPP
 #define UT_SWITCHBOARD_HPP
 
+// TODO(dk949): Clean up API:
+//                * Fix inconsistent naming
+//                * Hide things in `detail`
+//                * Look at all private/public accesses for exposed types
+// TODO(dk949): See if any of the `requires` can be turned into `static_assert`s for better errors
 
 #include <concepts>
 #include <cstdlib>
+#include <expected>
+#include <format>
 #include <functional>
 #include <optional>
+#include <span>
 #include <stdexcept>
 #include <type_traits>
+#include <utility>
 #if __cplusplus < 202'302L
 #    error this file has to be compiled with at least C++23
 #endif
 #include <ut/static_string/static_string.hpp>
+
+
+enum struct ParseMode { Short, Long, Positional };
+enum struct ParseErrorKind { MissingRequiredError, MissingArgError, FormatErorr, ConfigError };
+
+struct ParseError {
+    ParseErrorKind kind;
+    std::string message;
+};
+
+using ArgIterator = std::span<std::string_view>::iterator;
+
+template<typename T>
+struct NoVerifier {
+    std::optional<ParseError> verify(T const &) {
+        return std::nullopt;
+    }
+};
+
+template<typename T>
+struct SwitchParser {
+    std::expected<T, ParseError> parse(
+        ParseMode mode, int args_left, ArgIterator &current_argv, char short_mode_char, bool short_mode_last_in_group);
+    std::optional<ParseError> verify(T const &);
+};
+
+template<>
+struct SwitchParser<std::string_view> : NoVerifier<std::string_view> {
+    std::expected<std::string_view, ParseError> parse(ParseMode mode,  //
+        int args_left,
+        ArgIterator &current_argv,
+        char,
+        bool short_mode_last_in_group) {
+        switch (mode) {
+            case ParseMode::Short:
+                if (!short_mode_last_in_group) break;
+                [[fallthrough]];
+            case ParseMode::Long:
+                if (args_left == 1) break;
+                return *(current_argv++);
+            case ParseMode::Positional: return *current_argv;
+        }
+
+        return std::unexpected {
+            ParseError {ParseErrorKind::MissingArgError, std::format("Expected an argument for {}", *current_argv)}
+        };
+    }
+};
+
+template<>
+struct SwitchParser<std::string> : SwitchParser<std::string_view> {
+    std::expected<std::string, ParseError> parse(ParseMode mode,  //
+        int args_left,
+        ArgIterator &current_argv,
+        char short_mode_char,
+        bool short_mode_last_in_group) {
+        return SwitchParser<std::string_view>::parse(mode, args_left, current_argv, short_mode_char, short_mode_last_in_group)
+            .transform([](auto str) { return std::string {str}; });
+    }
+};
+
+template<typename T>
+struct SwitchParser<std::optional<T>> : SwitchParser<T> {
+    std::expected<std::string, ParseError> parse(ParseMode mode,  //
+        int args_left,
+        ArgIterator &current_argv,
+        char short_mode_char,
+        bool short_mode_last_in_group) {
+        return SwitchParser<T>::parse(mode, args_left, current_argv, short_mode_char, short_mode_last_in_group);
+    }
+};
 
 class SwitchError : public std::runtime_error {
     using std::runtime_error::runtime_error;
@@ -45,40 +125,62 @@ struct is_optional<std::optional<T>> : std::true_type { };
 template<typename T>
 inline constexpr bool is_optional_v = is_optional<T>::value;
 
+struct ArgConf {
+    bool operator==(ArgConf const &) const = default;
+};
+
+struct Help : ArgConf {
+    std::string_view help;
+
+    Help(std::string_view h)
+            : help {h} { }
+};
+
+struct Metavar : ArgConf {
+    std::string_view metavar;
+
+    Metavar(std::string_view m)
+            : metavar(m) { }
+
+    bool operator==(Metavar const &) const = default;
+};
+
+template<typename T>
+struct Default : ArgConf {
+    T def;
+
+    Default(T d)
+            : def(std::move(d)) { }
+};
+
 struct RegEntryKey {
     char s_flag;
     std::string_view l_flag;
+    Metavar metavar;
     bool operator==(RegEntryKey const &) const = default;
-};
-
-enum struct ParseMode { Short, Long, Positional };
-enum struct ParseErrorKind { OutOfArgsErorr, FormatErorr, Internal };
-
-struct ParseError {
-    ParseErrorKind kind;
-    std::string message;
 };
 
 struct RegEntry {
     std::function<std::optional<ParseError>(ParseMode mode,  //
-        int total_argc,
-        int &current_argc,
-        char **&current_argv,
+        int args_left,
+        ArgIterator &current_argv,
         char short_mode_char,
         bool short_mode_last_in_group)>
         parse;
-    std::function<bool()> verify;
+    std::function<std::optional<ParseError>()> verify;
+    std::optional<Help> help;
 };
 
 template<>
 struct std::hash<RegEntryKey> {
     std::size_t operator()(RegEntryKey const &key) const noexcept {
         return (std::hash<char> {}(key.s_flag) << 0)  //
-             ^ (std::hash<std::string_view> {}(key.l_flag) << 1);
+             ^ (std::hash<std::string_view> {}(key.l_flag) << 1)
+             ^ (std::hash<std::string_view> {}(key.metavar.metavar) << 2);
     }
 };
 
-struct Registry { };
+using Registry = std::unordered_map<RegEntryKey, RegEntry>;
 
 template<ut::StaticString name>
 struct NthNameBase {
@@ -96,15 +198,35 @@ struct NthName<n, first, rest...> : std::conditional_t<n == 0, NthNameBase<first
 template<std::size_t n, ut::StaticString... rest>
 static constexpr auto nthName = NthName<n, rest...>::value;
 
-struct ArgParserBase { };
+struct RegistryHolderBase {
+protected:
+    static bool hasKey(RegEntryKey) {
+        return false;
+    }
+};
 
-template<typename Parser, typename ParentParserT = ArgParserBase>
+template<typename Parser, typename ParentParserT = RegistryHolderBase>
 struct RegistryHolder : ParentParserT {
-    static Registry registry;
+    static inline Registry registry;
     using ThisParser = RegistryHolder<Parser, ParentParserT>;
     using ParentParser = ParentParserT;
 
-    static void addToRegistry(RegEntryKey key, RegEntry entry) { }
+    static bool hasKey(RegEntryKey key) {
+        return registry.contains(key) || ParentParserT::hasKey(key);
+    }
+
+    static void addToRegistry(RegEntryKey key, RegEntry entry) {
+        if (hasKey(key)) {
+            bool positional = key.s_flag == 0 && key.l_flag.empty();
+            throw ConfError(std::format("`{}{}{}{}` flag already added to registry",
+                key.s_flag,
+                !positional ? "/" : "",
+                key.l_flag,
+                positional ? "" : " ",
+                key.metavar.metavar));
+        }
+        registry.insert({key, entry});
+    }
 };
 
 template<typename T>
@@ -115,30 +237,6 @@ struct is_registry_holder<RegistryHolder<Parser, ParentParserT>> : std::true_typ
 
 template<typename T>
 concept registry_holder = is_registry_holder<T>::value;
-
-struct ArgConf { };
-
-struct Help : ArgConf {
-    std::string_view help;
-
-    Help(std::string_view h)
-            : help {h} { }
-};
-
-struct Metavar : ArgConf {
-    std::string_view metavar;
-
-    Metavar(std::string_view m)
-            : metavar(m) { }
-};
-
-template<typename T>
-struct Default : ArgConf {
-    T def;
-
-    Default(T d)
-            : def(std::move(d)) { }
-};
 
 struct Positional { };
 
@@ -185,7 +283,7 @@ public:
             : Arg(0, lflag, std::forward<Confs>(confs)...) { }
 
     template<std::derived_from<ArgConf>... Confs>
-    Arg(Positional, Confs &&...confs)
+    Arg(Positional, Confs &&...confs) requires(std::disjunction_v<std::is_same<Confs, Metavar>...>)
             : Arg(0, "", std::forward<Confs>(confs)...) { }
 
     operator T() {
@@ -195,12 +293,20 @@ public:
             return data.value();
     }
 
-    bool verify() {
+    std::optional<ParseError> verify() {
         // TODO(dk949): custom verifier support
-        if constexpr (std::is_same_v<T, bool> || is_optional_v<T>) {
-            return true;
-        } else {
-            return data.has_value();
+        if constexpr (std::is_same_v<T, bool>)
+            return std::nullopt;
+        else {
+            SwitchParser<T> parser;
+
+            if constexpr (is_optional_v<T>) {
+                if (!data.has_value()) return std::nullopt;
+                return parser.verify(*data);
+            } else {
+                if (!data.has_value()) return ParseError {ParseErrorKind::MissingRequiredError, std::format("")};
+                return parser.verify(*data);
+            }
         }
     }
 
@@ -226,26 +332,35 @@ private:
     }
 
     void addToRegistry() {
-        RegEntryKey key {s_flag, l_flag};
-        RegEntry entry {.parse = [&](ParseMode mode,
-                                     int total_argc,
-                                     int &current_argc,
-                                     char **&current_argv,
-                                     char short_mode_char,
-                                     bool short_mode_last_in_group) -> std::optional<ParseError> {
+        RegEntryKey key {s_flag, l_flag, metavar.value_or(Metavar {""})};
+        RegEntry entry {
+            .parse = [&](ParseMode mode,
+                         int args_left,
+                         ArgIterator &current_argv,
+                         char short_mode_char,
+                         bool short_mode_last_in_group) -> std::optional<ParseError> {
             if constexpr (std::is_same_v<T, bool>) {
                 switch (mode) {
                     case ParseMode::Short:
                     case ParseMode::Long: data = true; return std::nullopt;
                     case ParseMode::Positional:
-                        return ParseError {ParseErrorKind::Internal, "bool args cannot be positional"};
+                        return ParseError {ParseErrorKind::ConfigError, "bool args cannot be positional"};
+                    default:
+                        return ParseError {ParseErrorKind::ConfigError,
+                            std::format("Unexpected parse mode {}", std::to_underlying(mode))};
                 }
+            } else {
+                SwitchParser<T> parser;
+                if (auto val = parser.parse(mode, args_left, current_argv, short_mode_char, short_mode_last_in_group)) {
+                    data = val.value();
+                    return std::nullopt;
+                } else
+                    return val.error();
             }
-            return ParseError {ParseErrorKind::Internal, "not implemented"};
         },
-            .verify = [&]() {
-            return verify();
-        }};
+            .verify = [&]() { return verify(); },
+            .help = help,
+        };
         ParentRegHolder::addToRegistry(key, entry);
     }
 };
--- tests/switchboard.cpp
+++ tests/switchboard.cpp
@@ -9,6 +9,7 @@ UT_ARGS(MyArgs) {
 UT_ARGS(ChildArgs, MyArgs) {
     UT_ARG(bool) force_force {"--force-force", Help {"Really force some behaviour"}};
     UT_ARG(std::string_view) output {'o', Metavar {"FILE"}, Default {"a.out"}};
+    UT_ARG(std::string_view) input {positional, Metavar {"FILE"}, Default {"a.out"}};
 };
 
 TEST_CASE("Switch board smoke test", "[switchboard]") { }
