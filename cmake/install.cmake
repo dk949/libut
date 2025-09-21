303fd803e0f51f9c6dd99f0f55bab686f7ab4e3e
dk949 <56653556+dk949@users.noreply.github.com>
2025-09-22 00:06:52 +0100

    make sure package name matches cmake file names

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -4,31 +4,31 @@ include(CMakePackageConfigHelpers)
 function (libut_install)
     install(
         TARGETS ${LIBUT_TARGET_LIST}
-        EXPORT UTTargets
+        EXPORT utTargets
         FILE_SET HEADERS
         DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
     )
 
     install(
-        EXPORT UTTargets
-        NAMESPACE UT::
+        EXPORT utTargets
+        NAMESPACE ut::
         DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/ut
     )
 
     configure_package_config_file(
-        "${CMAKE_CURRENT_LIST_DIR}/cmake/UTConfig.cmake.in" "${CMAKE_CURRENT_BINARY_DIR}/UTConfig.cmake"
+        "${CMAKE_CURRENT_LIST_DIR}/cmake/utConfig.cmake.in" "${CMAKE_CURRENT_BINARY_DIR}/utConfig.cmake"
         INSTALL_DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/ut"
         NO_CHECK_REQUIRED_COMPONENTS_MACRO
     )
 
     write_basic_package_version_file(
-        "${CMAKE_CURRENT_BINARY_DIR}/UTConfigVersion.cmake"
+        "${CMAKE_CURRENT_BINARY_DIR}/utConfigVersion.cmake"
         VERSION ${PROJECT_VERSION}
         COMPATIBILITY AnyNewerVersion
     )
 
-    install(FILES "${CMAKE_CURRENT_BINARY_DIR}/UTConfig.cmake"
-                  "${CMAKE_CURRENT_BINARY_DIR}/UTConfigVersion.cmake"
+    install(FILES "${CMAKE_CURRENT_BINARY_DIR}/utConfig.cmake"
+                  "${CMAKE_CURRENT_BINARY_DIR}/utConfigVersion.cmake"
             DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/ut
     )
 endfunction ()
