d167782bb4b61d23657faaae7054eaf67ffe052c
dk949 <56653556+dk949@users.noreply.github.com>
2025-09-21 23:06:14 +0100

    updated how cmake files are generated

---
+++ cmake/targets.cmake
@@ -0,0 +1,14 @@
+macro (libut_define_target name)
+    add_library(${name} INTERFACE)
+    target_sources(
+        ${name}
+        PUBLIC FILE_SET HEADERS #
+               BASE_DIRS ${CMAKE_CURRENT_SOURCE_DIR}/include #
+               FILES ${ARGN}
+    )
+
+    target_include_directories(
+        ${name} INTERFACE $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/include> $<INSTALL_INTERFACE:include>
+    )
+    message(STATUS "libut: adding ${name}")
+endmacro ()
